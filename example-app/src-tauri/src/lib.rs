042b5f1cec2e18d788562238293dd973aae2d701
Alexey Alekhin <laughedelic@gmail.com>
2025-11-27 06:11:45 +0100

    cargo fmt

--- example-app/src-tauri/src/lib.rs
+++ example-app/src-tauri/src/lib.rs
@@ -7,8 +7,7 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     // Initialize logging
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info"))
-        .init();
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
     log::info!("Starting Tauri application");
     eprintln!("Starting Tauri application - this should print to stderr");
